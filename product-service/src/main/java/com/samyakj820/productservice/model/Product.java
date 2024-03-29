71f46006ffd33c96118c188716fdfc6a5c26e4ee
Samyak Jain <samyakj820@gmail.com>
2024-03-29 20:35:31 +0530

    Added Application configuration, Product model and Product request and response DTO

--- product-service/src/main/java/com/samyakj820/productservice/model/Product.java
+++ product-service/src/main/java/com/samyakj820/productservice/model/Product.java
@@ -1,15 +1,23 @@
 package com.samyakj820.productservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 }
