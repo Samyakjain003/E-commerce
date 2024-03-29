580386cb5c66bb444ba94136e0bea2ca8c74e156
Samyak Jain <samyakj820@gmail.com>
2024-03-30 01:26:56 +0530

    Added createProduct test case using TestContainers

--- product-service/src/test/java/com/samyakj820/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/samyakj820/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,69 @@
 package com.samyakj820.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.samyakj820.productservice.dto.ProductRequest;
+import com.samyakj820.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:5.7.34"));
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		String replicaSetUrl = mySQLContainer.getJdbcUrl() + "?replicaSet=myReplicaSet";
+		dynamicPropertyRegistry.add("spring.datasource.url", () -> replicaSetUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+	}
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString  = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+
+        Assertions.assertEquals(1, productRepository.findAll().size());
 	}
 
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Iphone 15")
+				.description("Iphone 15")
+				.price(BigDecimal.valueOf(150000))
+				.build();
+	}
 }
