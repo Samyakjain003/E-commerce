dff4081bdacc8d1d61796c8f865d06445aabbf92
Samyak Jain <samyakj820@gmail.com>
2024-03-29 19:12:07 +0530

    Added product service to the project

---
+++ product-service/src/main/java/com/samyakj820/productservice/model/Product.java
@@ -0,0 +1,15 @@
+package com.samyakj820.productservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
+public class Product {
+}
