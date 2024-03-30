97dc065df6741db36173aedf97ffa6c225cea14d
Samyak Jain <samyakj820@gmail.com>
2024-03-30 21:36:43 +0530

    Changed InventoryController request method to get all info of all the skuCodes

--- e-commerce-microservice/inventory-service/src/main/java/com/samyakj820/inventoryservice/controller/InventoryController.java
+++ e-commerce-microservice/inventory-service/src/main/java/com/samyakj820/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.samyakj820.inventoryservice.controller;
 
+import com.samyakj820.inventoryservice.dto.InventoryResponse;
 import com.samyakj820.inventoryservice.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,9 @@ public class InventoryController {
     @Autowired
     InventoryService inventoryService;
 
-    @GetMapping("/{skuCode}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public Boolean isInStock(@PathVariable String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 
--- e-commerce-microservice/inventory-service/src/main/java/com/samyakj820/inventoryservice/repository/InventoryRepository.java
+++ e-commerce-microservice/inventory-service/src/main/java/com/samyakj820/inventoryservice/repository/InventoryRepository.java
@@ -4,9 +4,12 @@ import com.samyakj820.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, String> {
     Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
